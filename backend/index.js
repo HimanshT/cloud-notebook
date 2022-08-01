631c4024d792dc9fbef002a663bac6c155ba8456
HimanshT <himanshutripathi4462@gmail.com>
2022-08-01 21:57:19 +0530

    delete notes done

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const connectToMongo = require('./db');
 const port = 5000
 connectToMongo();
 
 app.use(express.json());
+app.use(cors())
+
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
